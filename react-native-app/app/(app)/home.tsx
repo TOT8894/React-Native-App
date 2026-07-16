dd9ca749b0f961fd7acfbcc04b1a03e1bed27e4a
shanbel <shanbeldires7@gmail.com>
2026-07-16 09:56:39 +0300

    adding register and login router button

---
+++ react-native-app/app/(app)/home.tsx
@@ -0,0 +1,14 @@
+import { Pressable, View,Text } from "react-native";
+import {router} from "expo-router"
+export function Homepage(){
+    return(
+        <View>
+            <Pressable onPress={()=>router.push("/(auth)/registerScreen")}>
+                <Text>Go to register</Text>
+            </Pressable>
+            <Pressable onPress={()=>router.push("/(auth)/loginScreen")}>
+                 <Text>Go to login</Text>
+            </Pressable>
+        </View>
+    )
+}
