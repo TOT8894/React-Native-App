a0ec58be5b7f021ac0cca2f4fc3e347c433de7c3
shanbel <shanbeldires7@gmail.com>
2026-07-16 11:04:23 +0300

    adding gettoken function add token alias and error handling

--- react-native-app/src/storage/tokenStorage.ts
+++ react-native-app/src/storage/tokenStorage.ts
@@ -1,20 +1,34 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
-export const register=async({accessToken,refreshToken}:any)=>{
+interface token{
+    accessToken:string,
+    refreshToken:string
+}
+export const register=async({accessToken,refreshToken}:token):Promise<void>=>{
     await AsyncStorage.setItem("accessToken",accessToken)
     await AsyncStorage.setItem("refreshToken",refreshToken)
-    return {accessToken,refreshToken}
+   
 }
-export const login=async({accessToken,refreshToken}:any)=>{
+export const login=async({accessToken,refreshToken}:token):Promise<void>=>{
     await AsyncStorage.setItem("accessToken",accessToken)
     await AsyncStorage.setItem("refreshToken",refreshToken)
-    return {accessToken,refreshToken}
+    
 }
-export const logOut=async()=>{
+export const logOut=async():Promise<void>=>{
     await AsyncStorage.removeItem("accessToken")
     await AsyncStorage.removeItem("refreshToken")
 }
-export const checkLogin=async()=>{
+export const checkLogin=async():Promise<string>=>{
     const accessToken = await AsyncStorage.getItem("accessToken")
+    if(!accessToken){
+        return "accessToken is missing"
+    }
     return accessToken
+}
+export const getToken=async():Promise<token|null>=>{
+    const accessToken = await AsyncStorage.getItem("accessToken")
+    const refreshToken = await AsyncStorage.getItem("refreshToken")
+    if(!accessToken || !refreshToken){
+        return null
+    }
+    return {accessToken,refreshToken}
 }
