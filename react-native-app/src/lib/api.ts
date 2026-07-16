a2195b8b0cc3125fa9aa7b6aed4ce74a64ade769
shanbel <shanbeldires7@gmail.com>
2026-07-16 09:42:40 +0300

    adding login api

--- react-native-app/src/lib/api.ts
+++ react-native-app/src/lib/api.ts
@@ -1,4 +1,4 @@
-import { register } from "../storage/tokenStorage";
+import { login, register } from "../storage/tokenStorage";
 
 export const Register=async(data:any)=>{
     const response = await fetch("api/v1/auth/register",{
@@ -12,5 +12,19 @@ export const Register=async(data:any)=>{
     const accessToken = body?.accesstoken??body?.data?.accessToken;
     const refreshToken = body?.refreshToken??body?.data?.refreshToken;
     register({accessToken,refreshToken})
+    return body;
+}
+export const Login=async(data:any)=>{
+    const response = await fetch("api/v1/auth/login",{
+        method:"POST",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify(data)
+    })
+    const body = await response.json()
+    const accessToken = body?.accesstoken??body?.data?.accessToken;
+    const refreshToken = body?.refreshToken??body?.data?.refreshToken;
+    login({accessToken,refreshToken})
     return body;
 }
