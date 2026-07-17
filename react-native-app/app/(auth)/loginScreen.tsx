0c04b4db7bd975f8497be3cc2b7a4e02c54a9563
Shanbel Dires <shanbeldires7@gmail.com>
2026-07-17 11:10:10 +0300

    Refactor login handling and improve alerts

--- react-native-app/app/(auth)/loginScreen.tsx
+++ react-native-app/app/(auth)/loginScreen.tsx
@@ -1,11 +1,16 @@
 import { Pressable, TextInput,Alert, View,Text } from "react-native";
 import { useState } from "react";
 import { router } from "expo-router";
-import { Login } from "@/src/lib/api";
+import { useAuth } from "@/src/context/authContext";
+
 export default function LoginScreen():React.JSX.Element{
+
+    const {login} = useAuth()
     const [password,setPassword] = useState<string>("")
     const [email,setEmail] = useState<string>("")
-    function handle():void{
+    const [loading,setLoading] = useState<boolean>(false)
+
+    async function handle():Promise<void>{
         if(!email.trim()){
            Alert.alert("Error","email is required") 
            return    
@@ -18,36 +23,52 @@ export default function LoginScreen():React.JSX.Element{
            Alert.alert("Error","password should be at least 8 character")     
            return
         }
-        Alert.alert(
+        
+        const user = {
+            email:email.trim().toLowerCase(),
+            password:password.trim()
+        }
+        try{
+            setLoading(true)
+            await login(user)
+            Alert.alert(
             "Login",
             "Logged in successfully",
-           [    {text:"ok",onPress:()=>router.push("/")},
-                {text:"canclel",onPress:()=>router.push("/loginScreen")}
+           [    {text:"ok",onPress:()=>router.replace("/")}, 
             ]
         )
-        const user = {
-            email:email,
-            password:password
+        }catch(error:any){
+            Alert.alert("error",`${error?.message}`||"something gone wrong")
+        }finally{
+            setLoading(false)
         }
-        Login(user)
     }
     return(
         <View>
             <TextInput
                 placeholder="Enter email"
                 value={email}
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
                 onChangeText={setEmail}
             />
             <TextInput
                 placeholder="*******"
                 value={password}
                 secureTextEntry
+                autoCapitalize="none"
+                autoCorrect={false}
                 onChangeText={setPassword}
             />
             <Pressable
-                style={{borderBlockColor:"black",borderWidth:2,backgroundColor:"gray"}} 
+                style={{borderColor:"black",borderWidth:2,backgroundColor:"gray"}} 
+                disabled={loading}
                 onPress={handle}>
-                <Text style={{color: "white"}}>login</Text>
+                {loading?
+                    <Text style={{color: "white"}}>logging in...</Text>
+                    :<Text style={{color: "white"}}>login</Text>
+                }
             </Pressable>
         </View>
     )
