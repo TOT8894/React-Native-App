c9847baff344bd4c8c4632387fd1c26790e4cef7
shanbel <shanbeldires7@gmail.com>
2026-07-24 16:34:46 +0300

    creating footer component

---
+++ react-native-app/src/component/common/BottomNavigation.tsx.tsx
@@ -0,0 +1,28 @@
+import { View,Text } from "react-native";
+
+export default function Footer(){
+    return(
+        <>
+        <View>
+            <Text>
+                🏠 
+            </Text>
+            <Text>
+                🏨 
+            </Text>
+            <Text>
+                🗓️ 
+            </Text>
+            <Text>
+                🥂 
+            </Text>
+            <Text>
+                🤍 
+            </Text>
+            <Text>
+                ⋯ 
+            </Text>
+        </View>
+        </>
+    )
+}
