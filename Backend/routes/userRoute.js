bebb7e1e9c1c308d0334bcc3053c8cc5d612f478
Shanbel Dires <shanbeldires7@gmail.com>
2026-07-26 19:31:34 +0300

    Add updateUserStatus route to userRoute

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,7 +1,8 @@
 import Router from "express"
-import {getUser,getUserById,deleteUser,updateUser} from "./controllers/userController"
+import {getUser,getUserById,deleteUser,updateUser,updateUserStatus} from "./controllers/userController"
 const router = Router()
 router.get("/",getUser)
 router.get("/:id",getUserById)
 router.delete("/delete/:id",deleteUser)
 router.put("/update/:id",updateUser)
+router.patch("/id/status",updateUseStatus)
