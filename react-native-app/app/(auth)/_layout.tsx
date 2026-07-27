f84d813748d817cd98beb564a0da95c3e83c7d57
shanbel <shanbeldires7@gmail.com>
2026-07-27 11:50:28 +0300

    add auth provider to path auth components

--- react-native-app/app/(auth)/_layout.tsx
+++ react-native-app/app/(auth)/_layout.tsx
@@ -1,7 +1,10 @@
+import AuthProvider from "@/src/context/authContext";
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
   return (
+    <AuthProvider>
       <Stack screenOptions={{headerShown:false}} />
+    </AuthProvider>
   )
 }
