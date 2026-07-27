8f253c3f80bbe829c41e53d3e3d5b3a6385a0099
shanbel <shanbeldires7@gmail.com>
2026-07-27 12:34:54 +0300

    arranging home page and also add some styles

--- react-native-app/app/(tabs)/index.tsx
+++ react-native-app/app/(tabs)/index.tsx
@@ -4,61 +4,77 @@ export default function Homepage() {
   return (
     <SafeAreaView>
       <ScrollView>
-       <View>
-          <View>
-            <ImageBackground
-             style={{width:380,height:300,margin:5}}
-             source={require("../../assets/images/image.png")} />
-           
-          </View>
-          <View>
-            <View style={style.greeting_view}>
-                 <Text style={{fontWeight:"bold",fontSize:15, }}>Good Morning, Yared 🙋‍♂️</Text>
-                 <Text style={{fontWeight:"bold",fontSize:20, }}>Welcome to Genet Hotel !</Text>
-                <Text>Bole, Addis Ababa</Text>
+       <View style={style.main}>
+            <View>
+                <ImageBackground
+                style={{width:380,height:300,margin:5}}
+                source={require("../../assets/images/image.png")} />
+            
             </View>
-            <View style={style.greeting_view}>
-                <Text style={{color:"black",fontWeight:"bold"}}>what would you like to do?</Text>
-            </View>
+            
+                <View style={style.profile}>  
+                    <Text style={{fontWeight:"bold",fontSize:30,color:"white" }}>Good Morning, Yared 🙋‍♂️</Text>
+                    <Text style={{fontWeight:"bold",fontSize:20,color:"yellow" }}>Welcome to Genet Hotel !</Text>
+                </View>
+                
+                <View style={style.text}>
+                    <Text style={{fontWeight:"bold",fontSize:20,color:"black" }}>Bole, Addis Ababa</Text>
+                    <Text style={{color:"black",fontWeight:"bold"}}>what would you like to do?</Text>
+                </View>
+            
             <View style={style.flex}>
-                <View style={style.flex_box}>
-                    <Text>
+                <View >
+                    <Text style={style.flex_box}>
                         Book A room
                     </Text>
                 </View>
-                <View style={style.flex_box}>
-                    <Text>
+                <View >
+                    <Text style={style.flex_box}>
                         Order Food
                     </Text>
                 </View>
-                <View style={style.flex_box}>
-                    <Text>
+                <View>
+                    <Text style={style.flex_box}>
                         My Booking
                     </Text>
                 </View>
             </View>
-          </View>
        </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
 const style = StyleSheet.create({
+    main:{
+        display:"flex",
+        flexDirection:"column",
+        position:"relative",       
+    },
     flex:{
         display:"flex",
         flexDirection:"row",
         justifyContent:"space-around",
         gap:5,
         marginTop:20,
-
     },
     flex_box:{
-        backgroundColor:"pink",
+        backgroundColor:"green",
         padding:5,
-
+        color:"white",
+        fontWeight:600,
+        cursor:"pointer",
+        borderRadius:20,
+        paddingHorizontal:15,
+        paddingVertical:10,
     },
-    greeting_view:{
+    profile:{
         marginBottom:15,
-        alignItems:"center"
+        alignItems:"center", 
+        position:"absolute",
+        top:220,
+        left:20,
+    },
+    text:{
+        alignItems:"center",
     }
 })
