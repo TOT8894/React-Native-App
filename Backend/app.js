f0b9caefe101b3ec3655ed777ae857be72af6594
shanbel <shanbeldires7@gmail.com>
2026-07-28 10:25:01 +0300

    fix soime errors

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,12 @@
 import express from "express"
-import mongodb from "./config/mongodb"
-import errorHandler from "./errorHandler/errorHandler"
+import mongodb from "./config/mongodb.js"
+import {errorHandler} from "./errorHandler/errorHandler.js"
+import authRoute from "./routes/authRoute.js"
+import dotenv from "dotenv"
+dotenv.config()
 const app = express()
 mongodb()
-app.use("/auth",authRoute)
-app.use("/user",userRoute)
+app.use("/api/auth",authRoute)
+app.use("/api/user",userRoute)
 app.use(errorHandler)
 export default app
