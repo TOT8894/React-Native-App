a445a2989e10c6ed068054038a5b5f327f50f692
shanbel <shanbeldires7@gmail.com>
2026-07-28 10:27:03 +0300

    create error handler for backend app

---
+++ Backend/errorHandler/errorhandler.js
@@ -0,0 +1,13 @@
+export const errorHandler=(err,req,res,next)=>{
+    console.log("error has occured",err.stack);
+    res.status(500).json(
+        {
+            message:"error has occured",
+            success:false,
+            error:err.message
+        }
+    )
+    process.exit(1)
+    
+
+}
