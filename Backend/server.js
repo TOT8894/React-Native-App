30134f483cbc5e4604d7782f23edac66f715693a
shanbel <shanbeldires7@gmail.com>
2026-07-28 10:25:31 +0300

    importing port number

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
-import app from "./app"
-app.listen(prompt,()=>{
+import app from "./app.js"
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
     console.log(`server is running on port http://localhost:${PORT}`)
 })
